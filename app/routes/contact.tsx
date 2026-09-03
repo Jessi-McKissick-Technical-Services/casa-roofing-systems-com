365b593f1efcf53e25647c3582f9196938868569
Jessi McKissick <jessi@JMTS.dev>
2026-09-02 17:17:45 -0700

    Got the contact page layout setup including json for easy edits.

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
-import services from "../../JSON_data/contact.json";
+import contact_data from "../../JSON_data/contact.json";
+import roofing7 from "../assets/upscaled/roofing7.png";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,13 +12,16 @@ export function meta({}: Route.MetaArgs) {
 export default function Contact() {
   return (
     <div id="contact">
-        <p id="spiel">
+      <img src={roofing7}></img>
+      <p id="spiel">{contact_data.spiel}</p>
+      <div id="contact_plate">
+        <p id="phone_num"><b>Phone:</b> <b></b>{contact_data.contact_phone}</p>
+        <p id="email_addr"><b>Email:</b> {contact_data.contact_email}</p>
+        <p id="address_cont"><b>Address:</b> {contact_data.address}</p>
+      </div>
+      <p id="CTA">contact us today for your FREE quote!</p>
+      <iframe src={contact_data.map_embed} width="600" height="450" loading="lazy"></iframe>
 
-        </p>
-
-        <p id="phone_num">Phone: {services.contact_phone}</p>
-        <p id="email_addr">Email: {services.contact_email}</p>
-        <p id="address_cont">Address {services.address}</p>
     </div>
   );
 }
